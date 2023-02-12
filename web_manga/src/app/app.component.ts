0d7b309c7e48b65c9bac5041be35b14f03d40a67
Lê Đức Huy <leduchuy1461@gmail.com>
2023-02-12 08:49:03 +0700

    listen event when come and exist web

--- web_manga/src/app/app.component.ts
+++ web_manga/src/app/app.component.ts
@@ -1,11 +1,48 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, HostListener, OnDestroy } from '@angular/core';
 import { LoginService } from './login.service';
+import { OnlineUserService } from './online-user.service';
 
+class OnlineUser {
+  currentUser: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent{
+export class AppComponent {
   title = 'web_manga';
+
+  @HostListener('window:beforeunload', ['$event'])
+  async unloadHandler(event: BeforeUnloadEvent) {
+    // Perform any actions before the user closes the web page
+    debugger;
+    await this.updateOnlineUser('down');
+    debugger;
+  }
+
+  constructor(private onlineUserService: OnlineUserService) {
+    this.updateOnlineUser('up');
+  }
+  currentOnlineUser: OnlineUser;
+  async updateOnlineUser(action: string) {
+    // await this.onlineUserService
+    //   .getCurrentOnlineUser()
+    //   .toPromise()
+    //   .then((response) => {
+    //     this.currentOnlineUser = response;
+    //   });
+    await this.onlineUserService.getCurrentOnlineUser().subscribe((response) => {
+      this.currentOnlineUser = response;
+      if (action == 'up') {
+        this.currentOnlineUser!.currentUser++;
+      } else if (action == 'down') {
+        this.currentOnlineUser!.currentUser--;
+      }
+      debugger;
+      this.onlineUserService
+        .updateCurrentOnlineUser(this.currentOnlineUser)
+        .subscribe((response) => {});
+    });
+  }
 }
